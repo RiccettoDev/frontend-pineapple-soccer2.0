94887ecb398a7048563176e9c01505ac94b1a900
Eduardo <princesherlockk@gmail.com>
2024-04-29 19:52:47 -0300

    [frontend-pineappleSoccer]-fix- adjust ModalMatch

--- src/components/profile/ModalProfile.tsx
+++ src/components/profile/ModalProfile.tsx
@@ -7,35 +7,36 @@ import { MdEdit } from "react-icons/md";
 
 import FormEditProfile from "./FormEditProfile";
 
-export default function ModalProfile({ toggleModal }: any){
+export default function ModalProfile({ toggleModal }: any) {
   const [visible, setVisible] = useState(true)
   const [user, setUser] = useState<IUser | null>()
 
   const closeModal = () => {
     setVisible(false)
+    console.log(visible);
     toggleModal()
   };
 
   useEffect(() => {
-    const user =  getUserLocalStorage()
+    const user = getUserLocalStorage()
 
-    if(user) {
+    if (user) {
       setUser(user)
     }
   }, [])
 
-  return(
+  return (
     <>
       <div className="flex flex-col absolute z-40 w-[90%] h-[90%] lg:h-[1000px] -top-5 rounded-lg bg-slate-800">
         <button onClick={closeModal} className="text-lime-600 hover:text-white mt-4 ml-4">
-          <IoIosCloseCircle size={38}/>
+          <IoIosCloseCircle size={38} />
         </button>
         <div className="w-full h-full flex flex-col items-center mt-6">
           <div className="flex items-center justify-center bg-lime-700 shadow-xl w-[250px] h-[250px] rounded-full">
             <div className="relative">
-              <img src={user?.img} alt="Image Player" className="w-[250px] rounded-full"/>
+              <img src={user?.img} alt="Image Player" className="w-[250px] rounded-full" />
               <button className="text-white rounded-full bg-lime-600 p-1 absolute top-32 right-2 hover:scale-110">
-                <MdEdit size={38}/>
+                <MdEdit size={38} />
               </button>
             </div>
           </div>
