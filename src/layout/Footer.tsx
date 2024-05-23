6123ae8d332cd3ad520a3aace81fdb4028e2fdb4
Eduardo <princesherlockk@gmail.com>
2024-05-23 16:30:21 -0300

    [frontend-pineappleSoccer]-fix- adjust Footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,8 +1,8 @@
-export default function Footer(){
-  return(
+export default function Footer() {
+  return (
     <div className="flex justify-center items-center w-full h-10 bottom-0 custom:-bottom-[800px]">
       <a href="https://portfolio-ten-lime-67.vercel.app/" target="_blank">
-        <h2 className="text-lime-500 font-extrabold hover:scale-105 hover:text-white custom:text-4xl"> &copy; 2004 - Euardo Riccetto</h2>
+        <h2 className="text-lime-500 font-extrabold hover:scale-105 hover:text-white custom:text-4xl"> &copy; 2004 - Eduardo Riccetto</h2>
       </a>
     </div>
   )
