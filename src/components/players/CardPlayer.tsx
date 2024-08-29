c77cf97a50ab39820ccf9b3bd07cb54acfd72d62
Eduardo <princesherlockk@gmail.com>
2024-08-29 16:57:37 -0300

    [frontend-pineapleSoccer]-feat- created avaliation

--- src/components/players/CardPlayer.tsx
+++ src/components/players/CardPlayer.tsx
@@ -1,9 +1,31 @@
 import ShieldPlayers from "./ShieldPlayers";
 import Stars from "../profile/Stars";
 
-const PlayerStats = ({ playerProps }: any) => {
+interface PlayerProps {
+  id: string;
+  name: string;
+  surname: string;
+  force: string;
+  attack: string;
+  defense: string;
+  position: string;
+  goals: string;
+  assistance: string;
+  status: string;
+  stars: string;
+  img: string;
+  onVote: (player: PlayerProps) => void;
+}
+
+const PlayerStats = ({ playerProps }: { playerProps: PlayerProps }) => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col relative">
+      <button
+        className="w-20 h-10 bg-lime-800 absolute lg:-top-10 lg:left-52 -top-[4.5em] right-[13.5em] z-10 rounded-lg flex justify-center items-center hover:opacity-50"
+        onClick={() => playerProps.onVote(playerProps)}
+      >
+        <h1 className="text-white font-extrabold">Vote</h1>
+      </button>
       <h1 className="text-yellow-700 font-extrabold">
         <span className="text-lime-900 drop-shadow-lg shadow-black">Vigor:</span>{" "}
         {playerProps.force}%
@@ -36,7 +58,7 @@ const PlayerStats = ({ playerProps }: any) => {
   );
 };
 
-const CardPlayer = ({ playerProps }: any) => {
+const CardPlayer = ({ playerProps }: { playerProps: PlayerProps }) => {
   return (
     <div className="flex items-center w-full rounded-lg bg-yellow-400 lg:w-[500px]">
       <div className="p-4">
--- src/pages/Players.tsx
+++ src/pages/Players.tsx
@@ -1,25 +1,29 @@
-// Componente Players
-import { useState, useEffect } from "react"
-import CardPlayer from "../components/players/CardPlayer"
-import { Api } from "../services/api"
+import React, { useState, useEffect } from "react";
+import CardPlayer from "../components/players/CardPlayer";
+import { Api } from "../services/api";
+import { IoIosCloseCircle } from "react-icons/io";
+import ShieldPlayers from "../components/players/ShieldPlayers";
+import Stars from "../components/profile/Stars";
 
 interface PlayersProps {
-  id: string
-  name: string
-  surname: string
-  force: string
-  attack: string
-  defense: string
-  position: string
-  goals: string
-  assistance: string
-  status: string
-  stars: string
-  img: string
+  id: string;
+  name: string;
+  surname: string;
+  force: string;
+  attack: string;
+  defense: string;
+  position: string;
+  goals: string;
+  assistance: string;
+  status: string;
+  stars: string;
+  img: string;
 }
 
 export default function Players() {
   const [players, setPlayers] = useState<PlayersProps[]>([]);
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+  const [selectedPlayer, setSelectedPlayer] = useState<PlayersProps | null>(null);
 
   useEffect(() => {
     const fetchPlayers = async () => {
@@ -33,17 +37,75 @@ export default function Players() {
 
     setTimeout(() => {
       fetchPlayers();
-    }, 500)
+    }, 500);
   }, []);
+
+  const handleVoteClick = (player: PlayersProps) => {
+    setSelectedPlayer(player);
+    setModalIsVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalIsVisible(false)
+  };
 
   return (
     <div className="animate-slide-down flex flex-col items-center justify-center w-full mb-6 gap-4 pt-36 p-4">
       {players.length > 0 ? (
         players.map((player) => (
-          <CardPlayer key={player.id} playerProps={player} />
+          <CardPlayer key={player.id} playerProps={{ ...player, onVote: handleVoteClick }} />
         ))
       ) : (
         <p className="text-3xl text-white font-extrabold">Loading...</p>
+      )}
+      {modalIsVisible && selectedPlayer && (
+        <div className="bg-slate-700/80 p-2 w-full h-full fixed top-0 left-0 z-10 flex justify-center items-center">
+          <div className="bg-slate-800 w-[700px] h-[400px] flex justify-center items-center relative flex-col rounded-lg">
+            <button onClick={closeModal} className="text-lime-600 hover:text-white mt-4 ml-4 absolute top-0 right-5">
+              <IoIosCloseCircle size={38} />
+            </button>
+            <div className="flex gap-4 lg:gap-20">
+              <div className="mt-8">
+                <ShieldPlayers
+                  name1={selectedPlayer.name}
+                  name2={selectedPlayer.surname}
+                  img={selectedPlayer.img}
+                />
+                <Stars qtd={selectedPlayer.stars} size={25} color="#5a8a10" />
+              </div>
+              <div className="flex flex-col gap-4 p-4">
+                <h1 className="text-lime-500 font-extrabold">Habilidades:</h1>
+                <div className="flex">
+                  <h1 className="text-yellow-700 font-extrabold">
+                    <span className="text-lime-700 drop-shadow-lg shadow-black">Vigor:</span>{" "}
+                    <input type="text" placeholder={`${selectedPlayer.force}%`} className="w-14 bg-slate-700 rounded-lg text-center" />
+                  </h1>
+                </div>
+                <h1 className="text-yellow-700 font-extrabold">
+                  <span className="text-lime-700 drop-shadow-lg shadow-black">Ataque:</span>{" "}
+                  <input type="text" placeholder={`${selectedPlayer.attack}%`} className="w-14 bg-slate-700 rounded-lg text-center" />
+                </h1>
+                <h1 className="text-yellow-700 font-extrabold">
+                  <span className="text-lime-700 drop-shadow-lg shadow-black">Defesa:</span>{" "}
+                  <input type="text" placeholder={`${selectedPlayer.defense}%`} className="w-14 bg-slate-700 rounded-lg text-center" />
+                </h1>
+                <h1 className="text-yellow-700 font-extrabold">
+                  <span className="text-lime-700 drop-shadow-lg shadow-black">Chute:</span>{" "}
+                  <input type="text" placeholder={`${selectedPlayer.defense}%`} className="w-14 bg-slate-700 rounded-lg text-center" />
+                </h1>
+                <h1 className="text-yellow-700 font-extrabold">
+                  <span className="text-lime-700 drop-shadow-lg shadow-black">Passe:</span>{" "}
+                  <input type="text" placeholder={`${selectedPlayer.defense}%`} className="w-14 bg-slate-700 rounded-lg text-center" />
+                </h1>
+                <h1 className="text-yellow-700 font-extrabold">
+                  <span className="text-lime-700 drop-shadow-lg shadow-black">Cabeceio:</span>{" "}
+                  <input type="text" placeholder={`${selectedPlayer.defense}%`} className="w-14 bg-slate-700 rounded-lg text-center" />
+                </h1>
+                <button className="bg-lime-700 text-lime-50 rounded-lg hover:opacity-40">Salvar</button>
+              </div>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
